dac5d1bab1d6f79e1f1289bcce3d4de47c9222ce
HarryBrooks <harry_brooks@me.com>
2020-03-26 12:53:43 +0000

    Added routing

---
+++ music-app-client/src/config.js
@@ -0,0 +1,8 @@
+export default {
+    cognito: {
+        REGION: "eu-west-1",
+        USER_POOL_ID: "eu-west-1_DWhlOuzTz",
+        APP_CLIENT_ID: "3bdor0t8sfcsc0eps2h0b4ud9i",
+        IDENTITY_POOL_ID: "eu-west-1:e659b87a-7814-4ef3-aea9-912815cb8668"
+    }
+}
